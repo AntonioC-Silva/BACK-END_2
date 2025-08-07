06b4d8c6cf1f3842832c2ad1de83175a11a79371
AntonioC-Silva <acrscloud@gmail.com>
2025-08-07 16:44:19 -0300

    first API

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    data_nasc = models.DateField(null=True, blank=True)
+    naction = models.CharField(max_length=30, null=True, blank=True)
+    biogra = models.CharField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import Autor
+from .serializers import  AutorSerializers
+
+class AutoresView(ListCreateAPIView): #creat post  list get
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializers
+
 
 # Create your views here.
+
+
